b532384b3ed7ce432f4a7fd7eafec4336b9741a7
SCFC-CN <scfc_cn@outlook.com>
2025-01-19 21:56:10 +0800

    Site updated: 2025-01-19 21:56:09

---
+++ fps/fps.js
@@ -0,0 +1,18 @@
+if (window.localStorage.getItem("fpson") == undefined || window.localStorage.getItem("fpson") == "1") {
+    var rAF = function () {
+        return (
+            window.requestAnimationFrame ||
+            window.webkitRequestAnimationFrame ||
+            function (callback) {
+                window.setTimeout(callback, 1000 / 60);
+            }
+        );
+    }();
+    var frame = 0;
+    var allFrameCount = 0;
+    var lastTime = Date.now();
+    var lastFameTime = Date.now();
+    var loop = function () {
+        var now = Date.now();
+        var fs = (now - lastFameTime);
+        var fps = Math.round(1000 / fs);
